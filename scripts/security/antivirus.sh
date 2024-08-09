70d1ae8bb6e24a2c547b706f403f4cc6d3e7137b
sallfarr77 <sallfarr@outlook.com>
2024-08-10 00:43:05 +0700

    First commit

--- scripts/security/antivirus.sh
+++ scripts/security/antivirus.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-apt-get install -y clamav clamav-daemon
+systemctl stop clamav-freshclam
 
+# Update the ClamAV database
+rm -f /var/log/clamav/freshclam.log
 freshclam
+
+apt-get install -y clamav clamav-daemon
 
 # Create a scan script
 cat <<EOT > /etc/cron.daily/daily-clamav-scan
 #!/bin/bash
 
+# Log file location
 LOG_FILE="/var/log/clamav/daily-scan.log"
+
+# Create log file if not exist
 touch \$LOG_FILE
 
 # Scan the / directory excluding /proc, /sys, /dev, /run, /mnt, /media, /lost+found, and /tmp
-clamscan -r --exclude-dir="^/(proc|sys|dev|run|mnt|media|lost+found|tmp)" / > \$LOG_FILE
+clamscan -r --exclude-dir="^/(proc|sys|dev|run|mnt|media|lost+found|tmp)" / > \$LOG_FILE 2>&1
 EOT
 
 chmod +x /etc/cron.daily/daily-clamav-scan
+
+systemctl enable clamav-freshclam
+systemctl start clamav-freshclam
 
 echo "ClamAV installed and daily scan configured."
--- scripts/security/crontab.sh
+++ scripts/security/crontab.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Check if root has no crontab and create an empty one if necessary
+if ! crontab -l >/dev/null 2>&1; then
+    echo "# Empty crontab for root created" | crontab -
+fi
+
 # Configure cronjob to run the ClamAV scan script daily at 3 AM Jakarta time
 (crontab -l ; echo "0 3 * * * /etc/cron.daily/daily-clamav-scan") | crontab -
 
